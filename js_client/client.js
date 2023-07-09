9adfb46a173d299a6b9cf9873db766e6613e9ea7
anik-ghosh-au7 <tech.anikghosh@gmail.com>
2023-07-10 00:43:37 +0530

    updates

--- js_client/client.js
+++ js_client/client.js
@@ -14,39 +14,32 @@ rl.question("Enter the server url (example: localhost:8080): ", (serverURL) => {
   rl.question("Enter your client ID: ", (clientID) => {
     // Load the chat.proto file
     const packageDefinition = protoLoader.loadSync('../proto/chat.proto');
-
     // Load the chat service definition from the package definition
     const { main } = grpc.loadPackageDefinition(packageDefinition);
-
     // Connect to the gRPC server using the chat service
     const client = new main.ChatApi(
       serverURL,
       grpc.credentials.createInsecure()
     );
-
     // Create a new stream to the Connect method on the server
     const stream = client.Connect({ id: clientID });
-
     // Listen for 'data' events on the stream, which are triggered when messages are received from the server
     stream.on('data', (message) => {
       // Log incoming messages
       console.log(`${message.user.id}: ${message.content}`);
     });
-
     // Listen for 'error' events on the stream, which are triggered when an error occurs
     stream.on('error', (error) => {
       // Log the error and exit the process
       console.log('Disconnected from server due to error:', error.message);
       process.exit(1);
     });
-
     // Listen for 'end' events on the stream, which are triggered when the server ends the stream
     stream.on('end', () => {
       // Log the disconnection and exit the process
       console.log('Disconnected from server.');
       process.exit();
     });
-
     // Listen for 'line' events on the readline interface, which are triggered when the user enters a message
     rl.on('line', (line) => {
       // Send the message to the server using the Broadcast method
